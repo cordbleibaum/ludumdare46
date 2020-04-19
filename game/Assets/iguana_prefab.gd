e332695dbd6ecb478428fc95d15f9116679c880e
Cord Bleibaum <cordbleibaum@icloud.com>
2020-04-19 13:12:32 +0200

    walk animation

--- game/Assets/iguana_prefab.gd
+++ game/Assets/iguana_prefab.gd
@@ -1,16 +1,51 @@
-extends Spatial
+extends KinematicBody
+
+var gravity = -9.8
+var velocity = Vector3()
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var walkSpeed = 6
+export var ACCELERATION = 3
+export var DE_ACCELERATION = 5
 
+var anim
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	$Camera.make_current()
+	anim = get_parent().get_node("AnimationPlayer")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _physics_process(delta):
+	var dir = Vector3()
+
+	if(Input.is_action_pressed("move_forward")):
+		dir += transform.basis[2]
+
+	if(Input.is_action_pressed("move_left")):
+		rotate_y(2*delta)
+
+	if(Input.is_action_pressed("move_right")):
+		rotate_y(-2*delta)
+
+	dir.y = 0
+	dir = dir.normalized()
+
+	velocity.y += delta * gravity
+
+	var hv = velocity
+	hv.y = 0
+
+	var new_pos = dir * walkSpeed
+	var accel = DE_ACCELERATION
+
+	if (dir.dot(hv) > 0):
+		accel = ACCELERATION
+		
+	if hv.dot(hv) > 0.1:
+		if(not anim.is_playing()):
+			anim.play("walk")
+
+	hv = hv.linear_interpolate(new_pos, accel * delta)
+	velocity.x = hv.x
+	velocity.z = hv.z
+	velocity = move_and_slide(velocity, Vector3(0,1,0))	
