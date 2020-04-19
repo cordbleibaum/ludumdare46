c8816541c2e4120707144062041b37b74ef42416
Cord Bleibaum <cordbleibaum@icloud.com>
2020-04-19 13:44:26 +0200

    Sprint

--- game/Assets/iguana_prefab.gd
+++ game/Assets/iguana_prefab.gd
@@ -1,14 +1,18 @@
 extends KinematicBody
 
-var gravity = -9.8
-var velocity = Vector3()
-
-
+export var gravity = -9.8
+export var rotationSpeed = 3
 export var walkSpeed = 6
 export var acceleration = 3
 export var decceleration = 5
+export var sprintMultiplier = 2
+export var sprintEnergyConsumption = 0.3
+export var energyRegen = 0.1
 
 var anim
+var sprintEnergy = 1.0
+var velocity = Vector3()
+var energy = 1
 
 func _ready():
 	$Camera.make_current()
@@ -17,15 +21,24 @@ func _ready():
 
 func _physics_process(delta):
 	var dir = Vector3()
+	var multiplier = 1
+	var consumingEnergy = false
 
 	if(Input.is_action_pressed("move_forward")):
 		dir += transform.basis[2]
 
 	if(Input.is_action_pressed("move_left")):
-		rotate_y(2*delta)
+		rotate_y(rotationSpeed*delta)
 
 	if(Input.is_action_pressed("move_right")):
-		rotate_y(-2*delta)
+		rotate_y(-rotationSpeed*delta)
+
+	if(Input.is_action_pressed("sprint") && energy > 0):
+		multiplier *= sprintMultiplier
+		energy = energy - delta*sprintEnergyConsumption
+	
+	if(Input.is_action_pressed("sprint")):
+		consumingEnergy = true
 
 	dir.y = 0
 	dir = dir.normalized()
@@ -35,17 +48,26 @@ func _physics_process(delta):
 	var hv = velocity
 	hv.y = 0
 
-	var new_pos = dir * walkSpeed
+	var new_pos = dir * walkSpeed * multiplier
 	var accel = decceleration
 
 	if (dir.dot(hv) > 0):
 		accel = acceleration
 		
-	if hv.dot(hv) > 0.1:
+	if multiplier > 1 && new_pos.dot(new_pos) > 0.1:
+		anim.play("run")
+	elif new_pos.dot(new_pos) > 0.1:
 		if(not anim.is_playing()):
 			anim.play("walk")
+	else:
+		anim.stop()
 
 	hv = hv.linear_interpolate(new_pos, accel * delta)
 	velocity.x = hv.x
 	velocity.z = hv.z
 	velocity = move_and_slide(velocity, Vector3(0,1,0))	
+	
+	if not consumingEnergy:
+		energy = min(1.0, energy + delta*energyRegen)
+	else:
+		energy = max(0.0, energy)
