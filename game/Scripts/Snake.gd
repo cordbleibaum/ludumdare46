5fa3500f44e77e9131e0aea1720b809105fe2f31
Cord Bleibaum <cordbleibaum@icloud.com>
2020-04-20 17:05:00 +0200

    fix
    fix a crash

--- game/Scripts/Snake.gd
+++ game/Scripts/Snake.gd
@@ -1,9 +1,12 @@
 extends KinematicBody
 
 export var speed = 3
+export var gravity = -9.81
 
 var nav
 var player
+
+var velocity = Vector3(0, 0, 0)
 
 func _ready():
 	nav = get_tree().get_root().get_node("Spatial/Navigation")
@@ -12,10 +15,15 @@ func _ready():
 
 func _physics_process(delta):
 	var path = nav.get_simple_path(self.get_translation(), player.get_translation())
-	var nextPos = path[1]
-
-	var movement = (nextPos - transform.origin)
-	movement = movement.normalized() * speed 
-
-	move_and_slide(movement, Vector3(0,1,0))
-	look_at(player.get_translation(), Vector3(0,1,0))
+	
+	if path.size() > 0:
+		var nextPos = path[1]
+	
+		var movementTarget = (nextPos - transform.origin)
+		movementTarget = movementTarget.normalized() * speed
+		
+		velocity.x = movementTarget.x
+		velocity.z = movementTarget.z
+	
+		move_and_slide(movementTarget, Vector3(0,1,0))
+		look_at(player.get_translation(), Vector3(0,1,0))
