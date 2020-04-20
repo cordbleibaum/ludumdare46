3df71b96424e3fbebbc27cd282f7b0ac878a97d6
Cord Bleibaum <cordbleibaum@icloud.com>
2020-04-20 17:41:28 +0200

    beta snake

--- game/Scripts/Snake.gd
+++ game/Scripts/Snake.gd
@@ -6,7 +6,7 @@ export var gravity = -9.81
 var nav
 var player
 
-var velocity = Vector3(0, 0, 0)
+var velocity = Vector3()
 
 func _ready():
 	nav = get_tree().get_root().get_node("Spatial/Navigation")
@@ -14,7 +14,7 @@ func _ready():
 
 
 func _physics_process(delta):
-	var path = nav.get_simple_path(self.get_translation(), player.get_translation())
+	var path = nav.get_simple_path(self.get_translation()+Vector3(0,0.5,0), player.get_translation())
 	
 	if path.size() > 0:
 		var nextPos = path[1]
@@ -24,6 +24,7 @@ func _physics_process(delta):
 		
 		velocity.x = movementTarget.x
 		velocity.z = movementTarget.z
+		velocity.y += gravity*delta*0.1
 	
-		move_and_slide(movementTarget, Vector3(0,1,0))
+		velocity = move_and_slide(velocity, Vector3(0,1,0))
 		look_at(player.get_translation(), Vector3(0,1,0))
