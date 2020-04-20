147484e0c9e6dfa4d0670d69d9bdbf6f5560cde6
Cord Bleibaum <cordbleibaum@icloud.com>
2020-04-20 22:40:18 +0200

    reach ocean mesage

--- game/Scripts/Player.gd
+++ game/Scripts/Player.gd
@@ -15,7 +15,8 @@ var anim
 var sprintEnergy = 1.0
 var velocity = Vector3()
 var energy = 1
-
+var time_start = 0
+var started = false
 
 func _ready():
 	$Camera.make_current()
@@ -29,19 +30,34 @@ func movement(delta):
 
 	if(Input.is_action_pressed("move_forward")):
 		dir += transform.basis[2]
+		if not started:
+			started = true
+			time_start = OS.get_unix_time()
 
 	if(Input.is_action_pressed("move_left")):
 		rotate_y(rotationSpeed*delta)
+		if not started:
+			started = true
+			time_start = OS.get_unix_time()
 
 	if(Input.is_action_pressed("move_right")):
 		rotate_y(-rotationSpeed*delta)
+		if not started:
+			started = true
+			time_start = OS.get_unix_time()
 
 	if(Input.is_action_pressed("sprint") && energy > 0):
 		multiplier *= sprintMultiplier
 		energy = energy - delta*sprintEnergyConsumption
+		if not started:
+			started = true
+			time_start = OS.get_unix_time()
 	
 	if(Input.is_action_pressed("sprint")):
 		consumingEnergy = true
+		if not started:
+			started = true
+			time_start = OS.get_unix_time()
 
 	dir.y = 0
 	dir = dir.normalized()
@@ -83,5 +99,13 @@ func movement(delta):
 func _physics_process(delta):
 	if GameVariables.gameState == GameVariables.GAMESTATE.running:
 		movement(delta)
+		
+		if started:
+			var time_now = OS.get_unix_time()
+			var elapsed = time_now - time_start
+			
+			get_parent().get_node("MarginContainer/Label").text = str(int(elapsed))+"s"
+		else:
+			get_parent().get_node("MarginContainer/Label").text = "Reach the Ocean!"
 		
 	get_parent().get_node("MarginContainerEnergy/HBoxContainer/Label").text = str(int(energy*100))+"%"
